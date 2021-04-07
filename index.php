c02e81d78288ece8bb7d234c5eec6a45e6c46d5d
tutepp <79822854+tutepp@users.noreply.github.com>
2021-04-07 16:02:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include_once "../Lesson4/Product.php";
+include_once "../Lesson3/ManagerProduct.php";
+
+use Services\ProductManager;
+use Models\Product;
+
+$productManager = new ProductManager();
+$productManager->add(new Product("TV"));
+$productManager->add(new Product("PS6"));
+
+$products = $productManager->getProducts();
+foreach ($products as $product) {
+    echo $product->getName() ."<br/>";
+}
